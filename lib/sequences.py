5cfa49dadac59c03c5b80c3e3d69a2bd621fc5cc
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-05-20 01:11:36 +0300

    completed assignments

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     if length == 0:
+#         print([])
+
+#     elif length == 1:
+#         print([0])
+
+#     elif length == 2:
+#         print([0,1])
+
+#     elif length == 10:
+#         print 
+
+
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    print(fib_sequence)
